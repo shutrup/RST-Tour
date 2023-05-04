08e8312f1139c2c39d4efb8d13e5a08b6a47f737
shutrup <magagaraev95@mail.ru>
2023-05-04 19:27:59 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Шарап Бамматов on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
